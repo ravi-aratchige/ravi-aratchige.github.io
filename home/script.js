fae83b7a00c4c54285d56ddef4448dd6ad75295a
ravi-aratchige <rmaratchige@gmail.com>
2022-10-30 18:39:18 +0530

    Update script.js

--- home/script.js
+++ home/script.js
@@ -3,22 +3,25 @@
 let message, today = new Date()
 
 let time = today.getHours()
-time = 13   // for testing
-if (time < 12) {
-    message = 'Good Morning'
+time = 8   // for testing
+if (time < 5) {
+    document.querySelector('#greeting').innerHTML = 'Late Night?'
+} else if (time < 12) {
+    document.querySelector('#greeting').innerHTML = 'Good Morning'
 } else if (time < 15) {
-    message = 'Good Afternoon'
+    document.querySelector('#greeting').innerHTML = 'Good Afternoon'
 } else if (time < 22) {
-    message = 'Good Evening'
+    document.querySelector('#greeting').innerHTML = 'Good Evening'
 } else if (time < 24) {
-    message = 'Late Night?'
+    document.querySelector('#greeting').innerHTML = 'Late Night?'
 }
-
-document.querySelector('#greeting').innerHTML = message
 
 // Setting background gradients according to the time of the day
 
-if (time < 12) {
+if (time < 5) {
+    // early morning gradient
+    document.querySelector('body').style.background="linear-gradient(-45deg, #767d92, #2f4562, #152642, #081b33)";
+} else if (time < 12) {
     // morning gradient
     document.querySelector('body').style.background="linear-gradient(-45deg, #fa4e5e, #f9a9e, #99b9d0, #00b2d2)";
 } else if (time < 15) {
@@ -31,6 +34,8 @@ if (time < 12) {
     // night gradient
     document.querySelector('body').style.background="linear-gradient(-45deg, #767d92, #2f4562, #152642, #081b33)";
 }
+
+// animating gradients
 
 document.querySelector('body').style.backgroundSize="400% 400%";
 document.querySelector('body').style.animation="gradient 8s ease infinite";
