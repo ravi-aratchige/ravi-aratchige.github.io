72420541c30f84e394ef41ca30351f1b4d0e940a
ravi-aratchige <rmaratchige@gmail.com>
2022-11-09 19:13:17 +0530

    Minor changes

--- home/script.js
+++ home/script.js
@@ -20,22 +20,22 @@ if (time < 5) {
 
 if (time < 5) {
     // early morning gradient
-    document.querySelector('body').style.background="linear-gradient(-45deg, #767d92, #2f4562, #152642, #081b33)";
+    document.getElementsByTagName('body').style.background="linear-gradient(-45deg, #767d92, #2f4562, #152642, #081b33)";
 } else if (time < 12) {
     // morning gradient
-    document.querySelector('body').style.background="linear-gradient(-45deg, #fa4e5e, #f9a9e, #99b9d0, #00b2d2)";
+    document.getElementsByTagName('body').style.background="linear-gradient(-45deg, #fa4e5e, #f9a9e, #99b9d0, #00b2d2)";
 } else if (time < 15) {
     // afternoon gradient
-    document.querySelector('body').style.background="linear-gradient(-45deg, #96adcf, #4570b5, #234181, #1b2966)";
+    document.getElementsByTagName('body').style.background="linear-gradient(-45deg, #96adcf, #4570b5, #234181, #1b2966)";
 } else if (time < 22) {
     // evening gradient
-    document.querySelector('body').style.background="linear-gradient(-45deg, #fdec6e, #e0943d, #bc361a, #391106)";
+    document.getElementsByTagName('body').style.background="linear-gradient(-45deg, #fdec6e, #e0943d, #bc361a, #391106)";
 } else if (time < 24) {
     // night gradient
-    document.querySelector('body').style.background="linear-gradient(-45deg, #767d92, #2f4562, #152642, #081b33)";
+    document.getElementsByTagName('body').style.background="linear-gradient(-45deg, #767d92, #2f4562, #152642, #081b33)";
 }
 
 // animating gradients
 
-document.querySelector('body').style.backgroundSize="400% 400%";
-document.querySelector('body').style.animation="gradient 8s ease infinite";
+document.getElementsByTagName('body').style.backgroundSize="400% 400%";
+document.getElementsByTagName('body').style.animation="gradient 8s ease infinite";
